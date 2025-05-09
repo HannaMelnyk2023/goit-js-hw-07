bc3745fd26b1e1b76d04a401ebacff2c4933af1d
HannaMelnyk2023 <151830548+HannaMelnyk2023@users.noreply.github.com>
2025-05-09 15:42:54 +0300

    first  part of HW
    adding gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
+const galleryList = document.querySelector(".gallery");
+// const markup = galleryItems.map(item => `<li><img src=${item.preview} alt="smth"/> <p>${item.description}</p></li>`).join("");
+function markup(arr) {
+    return arr.map(
+        ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    ).join("");
+}
+galleryList.insertAdjacentHTML("beforeend", markup(galleryItems));
 console.log(galleryItems);
