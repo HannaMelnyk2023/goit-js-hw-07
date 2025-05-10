8a33356c2ba2afb4b860d9c70aad08d5718c3605
HannaMelnyk2023 <151830548+HannaMelnyk2023@users.noreply.github.com>
2025-05-10 21:59:30 +0300

    1 part is finished
    MODAL!!!! DONE
    ESC too

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,10 +1,11 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 const galleryList = document.querySelector(".gallery");
-galleryList.addEventListener(onclick)
+
 function markup(arr) {
-    return arr.map(
-        ({ preview, original, description }) => `<li class="gallery__item">
+  return arr
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
@@ -14,12 +15,44 @@ function markup(arr) {
     />
   </a>
 </li>`
-    ).join("");
+    )
+    .join("");
 }
 galleryList.insertAdjacentHTML("beforeend", markup(galleryItems));
-console.log(galleryItems);
-// чернетка:
-const instance = basicLightbox.create(`
-    <p>You can set the content of the lightbox with JS.</p>
-`)
-// закриття по esc див testModule6R_2 рисіч
+
+galleryList.addEventListener("click", onclick);
+function onclick(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  const urlOriginalImg = evt.target.dataset.source;
+  // console.log(urlOriginalImg);
+
+  const options = {
+    onShow: (instance) => {
+      document.addEventListener("keydown", escModalClosed);
+    },
+
+    onClose: (instance) => {
+      document.removeEventListener("keydown", escModalClosed);
+    },
+  };
+  const instance = basicLightbox.create(
+    `
+     <img
+      class="gallery__image"
+      src='${urlOriginalImg}'
+      alt='${urlOriginalImg}'
+    />`,
+    options
+  );
+  instance.show(() => console.log("lightbox now visible"));
+  function escModalClosed(evt) {
+    console.log(evt.code);
+    if (evt.code === "Escape") {
+      instance.close(() => console.log("HOHOHO you press ESC! lightbox now NOT visible"));
+    }
+  }
+}
+// закриття по esc див testModule6R_2 RYSICH
